b4dc801b8004b57cdece37a1d3a38a5d64d6a74b
kristian-wittrup <kw@easv.dk>
2023-03-02 08:55:28 +0100

    updated 06-menu

--- 06-menu/final/app.js
+++ 06-menu/final/app.js
@@ -81,33 +81,37 @@ const menu = [
   },
 ];
 
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");                    // PART II
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu);
+  //displayMenuButtons();       //  This is the function that creates the buttons // PART II
+});
 
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
 
+    return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  // console.log(displayMenu);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  sectionCenter.innerHTML = displayMenu;
+} 
 
 /* 
 
